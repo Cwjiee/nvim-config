6422d41b62a44e2c24d2e78c4fd0a880b965ee65
Wei Jie <jackchong398@gmail.com>
2023-09-29 23:38:47 +0800

    feat: go lsp config

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -1,4 +1,5 @@
 local lsp_zero = require('lsp-zero')
+local util = require('lspconfig/util')
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
@@ -13,5 +14,22 @@ require('mason-lspconfig').setup({
   ensure_installed = {'gopls', 'rubocop'},
   handlers = {
     lsp_zero.default_setup,
+    gopls = function()
+	    require('lspconfig').gopls.setup({
+		cmd = {"gopls"},
+		filetypes = {"go", "gomod", "gowork", "gotmpl" },
+		root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+		settings = {
+			gopls = {
+				completeUnimported = true,
+				usePlaceholders = true,
+				analyses = {
+					unusedparams = true,
+				},
+				gofumpt = true
+			}
+		}
+	    })
+    end,
   },
 })

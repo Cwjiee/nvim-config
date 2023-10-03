6c4205a26b2f07486008b528beb12097083059d1
Wei Jie <jackchong398@gmail.com>
2023-10-03 13:48:03 +0800

    feat: cmd config

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -7,29 +7,39 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  -- Replace the language servers listed here 
-  -- with the ones you want to install
-  ensure_installed = {'gopls', 'rubocop'},
-  handlers = {
-    lsp_zero.default_setup,
-    gopls = function()
-	    require('lspconfig').gopls.setup({
-		cmd = {"gopls"},
-		filetypes = {"go", "gomod", "gowork", "gotmpl" },
-		root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-		settings = {
-			gopls = {
-				completeUnimported = true,
-				usePlaceholders = true,
-				analyses = {
-					unusedparams = true,
-				},
-				gofumpt = true
-			}
+
+require('lspconfig').gopls.setup({
+	cmd = {"gopls"},
+	filetypes = {"go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
 		}
-	    })
-    end,
-  },
+	}
+})
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  })
 })

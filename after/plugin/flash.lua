2414743a7614ccdc64aa4390721cd3f6451736d2
weijie <jackchong398@gmail.com>
2024-07-19 16:32:43 +0800

    feat: add keymaps for tabs

--- after/plugin/flash.lua
+++ after/plugin/flash.lua
@@ -1,4 +1,3 @@
-vim.keymap.set("n", "f", function() require("flash").jump() end)
-vim.keymap.set("c", "<C-s>", function() require("flash").toggle() end)
+vim.keymap.set({ "n", "x", "o"}, "s", function() require("flash").jump() end)
 vim.keymap.set({ "o", "x" }, "<C-f>", function() require("flash").treesitter_search() end)
 
